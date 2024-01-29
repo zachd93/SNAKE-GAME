2e538439a48fe7a70d4c8e4ce0adb54f98a23daa
zachd93 <zachdebolt1993@gmail.com>
2024-01-29 12:44:00 -0800

    Added functions

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Define HTML elements
+const board = document.getElementById('game-board');
+
+// Define game variables
+let snake = [{ x: 10, y: 10 }];
+
+// Draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div', 'snake');
+    })
+}
+
+// Create a snake or food cube/div
+function createGameElement(tag, className) {
+    const element = document.createElemen(tag);
+}
